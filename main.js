bc6635c9cd8339d784044ccc89e8cf221e9f4129
yashmahesh <86894072+yashmahesh@users.noreply.github.com>
2021-07-06 19:22:25 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 // openweathermap.org
-let my_city = "London";
-let API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${my_city}&appid=0c064be1a54bc58da877f505c0940ac5&units=metric`;
+let my_city; //= "London";
+let API_URL ;//= `https://api.openweathermap.org/data/2.5/weather?q=${my_city}&appid=0c064be1a54bc58da877f505c0940ac5&units=metric`;
 
 const main = document.getElementById("main");
 const form = document.getElementById("form");
 const search = document.getElementById("search");
-
+const clk =document.getElementById("clk");
 // console.log(API_URL);
-async function getCity(cityname){
+ async function getCity(cityname){
     my_city = cityname;
     API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${my_city}&appid=0c064be1a54bc58da877f505c0940ac5&units=metric`;
-    const resp = await fetch(API_URL);
+    const resp =  await fetch(API_URL);
     // console.log(resp);
     if(resp.status == 200){
         const respData = await resp.json();
@@ -22,38 +22,50 @@ async function getCity(cityname){
 }
 
 function createCityCard(city){
-    let iconCode = `${city.weather[0].icon}`;
-    console.log(iconCode);
+    let iconCode = city.weather[0].icon;
+    //console.log(iconCode);
     
     document.body.style.backgroundImage = `url(./images/${iconCode[0] + iconCode[1] + 'd'}.jpeg)`;
-    document.body.style.backgroundSize = "cover";
+    document.body.style.backgroundSize = "100vw 100vh";
     
     let iconURL = `http://openweathermap.org/img/wn/${iconCode}@2x.png`;
     // console.log(iconURL);
-    const cardHTML = `
-    <div>
-        <ul>
-            <li><strong>City Name:</strong> ${city.name} </li>
-            <li><strong>Country Name: </strong> ${city.sys.country} </li>
-            <br>
-            <li><strong>Weather: </strong> ${city.weather[0].main} </li>
-            <li><strong>Weather Description: </strong> ${city.weather[0].description} </li>
-            <br>
-            <li><strong>Temperature:</strong> ${city.main.temp} <span>&#8451;</span></li>
-            <li><strong>Feels like:</strong> ${city.main.feels_like} <span>&#8451;</span></li>
-        </ul>
-        <img src = ${iconURL} alt = "Weather Icon">
-    </div>
-    `;
-    main.innerHTML = cardHTML;
-}
+       
+            cityname.innerHTML=`<strong>City Name:</strong> ${city.name}`
+            console.log("city entered",city.name);
 
-form.addEventListener("submit", e => {
-    e.preventDefault();  
-    const city = search.value;
-    console.log("You searched for city", city);
-    if(city){
-        getCity(city);
-        search.value = "";
+            country.innerHTML=`<strong>Country Name: </strong> ${city.sys.country}` 
+    
+            weather.innerHTML=`<strong>Weather: </strong> ${city.weather[0].main}`
+
+            desc.innerHTML=`<strong>Weather Description: </strong> ${city.weather[0].description}`
+
+                 temp.innerHTML=`<strong>Temperature:</strong> ${city.main.temp} <span>&#8451;</span>`
+                        feel.innerHTML=`<strong>Feels like:</strong> ${city.main.feels_like} <span>&#8451;</span>`
+        
+        image.innerHTML=`<img src = ${iconURL} alt = "Weather Icon" id="icon">`
+       
     }
-});
+
+clk.addEventListener("click",findtext);
+
+         //   event.preventDefault();
+         function findtext()
+         {   let my_cit=search.value;
+            console.log("your sarch",my_cit);
+            if(my_cit){
+                getCity(my_cit);
+                search.value="";
+            }
+        }
+ form.addEventListener("submit", function(event) {
+     event.preventDefault();  
+     const city = search.value;
+     console.log("You searched for city", city);
+     if(city){
+         getCity(city);
+         search.value = "";
+    }
+ }
+
+ );
